3b39b78bb70174c11591db470a83dfd8c57b94ac
Karl Huber <Karl Huber@KarlsPC.fritz.box>
2018-06-13 08:02:30 +0200

    stunArrow and fireball finished

---
+++ src/de/saginfo/mazehunter/game/player/abilities/projectiles/TrapProjectile.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.saginfo.mazehunter.game.player.abilities.projectiles;
+
+/**
+ *
+ * @author Karl Huber
+ */
+public class TrapProjectile {
+    
+}
